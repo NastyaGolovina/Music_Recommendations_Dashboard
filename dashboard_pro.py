ddeb263e8850f1fbeed8eb32838977d551353ab4
Anastasiia <n21koketka@gmail.com>
2026-03-18 16:20:12 +0200

    update dashboard_pro.py add metric

--- dashboard_pro.py
+++ dashboard_pro.py
@@ -8,3 +8,10 @@ def pro_dashboard():
 
     with st.spinner("Loading music data..."):
         music = load_music()
+
+    c1, c2, c3, c4 = st.columns(4)
+
+    c1.metric("Songs", music["title"].nunique(), border=True)
+    c2.metric("Artists", music["artist"].nunique(), border=True)
+    c3.metric("Regions", music["region"].nunique(), border=True)
+    c4.metric("streams", music['streams'].sum(), border=True)
