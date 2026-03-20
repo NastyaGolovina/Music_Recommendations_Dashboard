47f6975c8c3c1527550dc84c2e7be82bfe787e06
Anastasiia <n21koketka@gmail.com>
2026-03-20 13:35:53 +0200

    dashboard structure

--- dashboard_pro.py
+++ dashboard_pro.py
@@ -15,3 +15,24 @@ def pro_dashboard():
     c2.metric("Artists", music["artist"].nunique(), border=True)
     c3.metric("Regions", music["region"].nunique(), border=True)
     c4.metric("streams", music['streams'].sum(), border=True)
+
+
+
+    col_left, col_right = st.columns([2, 1] , border=True)
+
+    with col_left:
+        r1, r2 = st.columns([3, 2], border=True)
+        with r1:
+            st.subheader("Upper left left")
+        with r2:
+            st.subheader("Left upper right")
+
+        r3, r4 = st.columns([1, 3], border=True)
+        with r3:
+            st.subheader("Left lower left")
+        with r4:
+            st.subheader("Left lower right")
+
+
+    with col_right:
+        st.subheader("Right column")
--- dashboard_user.py
+++ dashboard_user.py
@@ -18,3 +18,24 @@ def user_dashboard():
     c2.metric("Artists", music["artist"].nunique(), border=True)
     c3.metric("Regions", music["region"].nunique(), border=True)
     c4.metric("streams", music['streams'].sum(), border=True)
+
+
+    col_left, col_right = st.columns([1, 2] , border=True)
+
+    with col_left:
+        st.subheader("Left column")
+
+    with col_right:
+
+        r1, r2 = st.columns([3, 2], border=True)
+        with r1:
+            st.subheader("Upper right left")
+        with r2:
+            st.subheader("Right upper right")
+
+
+        r3, r4 = st.columns([5, 6], border=True)
+        with r3:
+            st.subheader("Right lower left")
+        with r4:
+            st.subheader("Right lower right")
