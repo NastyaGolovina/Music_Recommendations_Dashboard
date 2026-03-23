15cd16d73cd1203b2f88981d0dccdf301c7e20b2
Anastasiia <n21koketka@gmail.com>
2026-03-23 22:47:31 +0200

    Ranking of songs

--- dashboard_pro.py
+++ dashboard_pro.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from data_loader import load_data
+import pandas as pd
+import plotly.express as px
+import altair as alt
+
 
 
 
@@ -57,7 +61,63 @@ def pro_dashboard():
         with r3:
             st.subheader("Left lower left")
         with r4:
-            st.subheader("Left lower right")
+            st.subheader("Ranking of songs")
+            songs_ranking = music.copy()
+
+            col1, col2, col3 = st.columns(3)
+
+            songs_ranking["date"] = pd.to_datetime(songs_ranking["date"])
+
+            song = col1.selectbox(
+                "Song",
+                ["Rockabye (feat. Sean Paul & Anne-Marie)"] +
+                songs_ranking[
+                    songs_ranking["title"] != "Rockabye (feat. Sean Paul & Anne-Marie)"
+                ]["title"].unique().tolist()
+            )
+            with col2:
+                d = st.date_input(
+                    "Duration",
+                    value=(songs_ranking["date"].min().date(), songs_ranking["date"].max().date()),
+                    min_value=songs_ranking["date"].min().date(),
+                    max_value=songs_ranking["date"].max().date(),
+                    format="DD.MM.YYYY",
+                )
+
+            region = col3.selectbox(
+                "Region",
+                ["Global"] + songs_ranking[songs_ranking["region"] != "Global"]["region"].unique().tolist()
+            )
+
+            if len(d) == 2:
+                songs_ranking = songs_ranking[
+                    (songs_ranking["date"] >= pd.Timestamp(d[0])) &
+                    (songs_ranking["date"] <= pd.Timestamp(d[1]))
+                    ]
+
+            songs_ranking = songs_ranking[songs_ranking["region"] == region]
+            songs_ranking = songs_ranking[songs_ranking["title"] == song]
+
+            songs_ranking = songs_ranking.sort_values(by='date', ascending=True)
+
+            # st.line_chart(
+            #     songs_ranking,
+            #     x="date",
+            #     y=["rank"],
+            #     color=["#FF0000"],
+            # )
+            line = alt.Chart(songs_ranking).mark_line(color="#7777ff").encode(
+                x="date:T",
+                y=alt.Y("rank:Q", scale=alt.Scale(reverse=True), title="Rank")
+            )
+
+            # points = alt.Chart(songs_ranking).mark_point(color="#7777ff", size=80, filled=False).encode(
+            points = alt.Chart(songs_ranking).mark_point(color="#7777ff", size=80, filled=True).encode(
+                x=alt.X("date:T", title="Date"),
+                y=alt.Y("rank:Q", scale=alt.Scale(reverse=True))
+            )
+
+            st.altair_chart((line + points).interactive(), use_container_width=True)
 
 
     with col_right:
